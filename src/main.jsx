3c33a376279f0e2942d7483f97ab586aff99b61f
IgorSMiada <igor.miada@gmail.com>
2025-10-21 18:49:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,54 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import PaginaCadastro from './components/cadastro/PaginaCadastro.jsx';
-import PaginaLogin from './components/login/paginaLogin.jsx';
+import PaginaLogin from './components/login/PaginaLogin.jsx';
 import Principal from './components/principal/PagPrincipal.jsx';
 import PaginaPerfil from './components/perfil/PaginaPerfil.jsx';
 import AdicionarPet from './components/perfil/AdicionarPet.jsx';
-import ChatConversa from './components/principal/ChatConversa.jsx'
-import ListaAmigos from './components/amigos/ListaAmigos.jsx';
-import PerfilPublico from './components/perfil/PerfilPublico';
+import ChatConversa from './components/principal/ChatConversa.jsx';
+
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>,
+    path: "/",
+    element: <App/>,
   },
   {
-    path:"/cadastro",
-    element:<PaginaCadastro/>,
+    path: "/cadastro",
+    element: <PaginaCadastro/>,
   },
   {
-    path:"/login",
-    element:<PaginaLogin/>,
+    path: "/login",
+    element: <PaginaLogin/>,
   },
   {
-    path:"/principal",
-    element:<Principal/>,
+    path: "/principal",
+    element: <Principal/>,
   },
   {
-    path:"/perfil",
-    element:<PaginaPerfil/>,
+    path: "/perfil",
+    element: <PaginaPerfil/>,
   },
   {
-  path:"/perfil/adicionar-pet",
-  element: <AdicionarPet />,
-  }, 
-  {
-    path:"/principal/amigos",
-    element: <ListaAmigos />,
+    path: "/perfil/adicionar-pet",
+    element: <AdicionarPet />,
   },
   {
-  path:'/chat',
-  element:<ChatConversa />,
-  },
-  {
-    path:'/perfil/publico/:id',
-    element:<PerfilPublico />
+    path: "/chat/:amigoId",
+    element: <ChatConversa />,
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
